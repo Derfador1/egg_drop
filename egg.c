aa71e66aa82498eb9b35ab5cbc18c91d0b23e1fc
Derfador1 <derfador1@gmail.com>
2016-02-05 07:52:09 -0500

    given code for egg.c

---
+++ egg.c
@@ -0,0 +1,30 @@
+#include "egg.h"
+
+static const int SECRET_FLOOR_LIMIT = 10;
+
+egg *lay_egg(void)
+{
+	egg *e = malloc(sizeof(*e));
+	if(e) {
+		e->id = SECRET_FLOOR_LIMIT;
+	}
+	
+	return e;
+}
+
+void cook_egg(egg *e)
+{
+	free(e);
+}
+
+bool egg_is_broken(egg *e)
+{
+	return e->id == 0;
+}
+
+void egg_drop_from_floor(egg *e, size_t floor)
+{
+	if(e && (size_t)e->id < floor) {
+		e->id = 0;
+	}
+}
