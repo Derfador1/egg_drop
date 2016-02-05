9c12be30bbfa1ddd2cf678400bd3040cd9857e4d
Derfador1 <derfador1@gmail.com>
2016-02-05 09:05:30 -0500

    working on main loop to drop eggs

--- egg_drop.c
+++ egg_drop.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <string.h>
 
+size_t pick_floor(int eggs, int floor);
+
 int main(int argc, char *argv[])
 {
 	int floors = 0;
@@ -29,10 +31,41 @@ int main(int argc, char *argv[])
 		carton[counter] = lay_egg();
 	}
 
+	size_t guess = pick_floor(eggs, floors);
+
+	while(eggs != 0) {
+		printf("guess: %zd\n", guess);
+		
+		egg_drop_from_floor(carton[eggs - 1], guess);
+
+		if(egg_is_broken(carton[eggs - 1])) {
+			printf("EGG CRACKED at floor %ld\n", guess);
+			eggs--;
+		}
+		else {
+			printf("EGG SURVIVED at floor %ld\n", guess);
+		}
+
+		guess = pick_floor(eggs, guess);
+	}
+
+	//free error here
 	for (counter = 0; counter < eggs; counter++) {
 		cook_egg(carton[counter]);
 	}
 	
-	free(carton);
+	free(*carton);
 	//only frees everything if make debug is called
+}
+
+size_t pick_floor(int eggs, int floor)
+{
+	if(eggs == 1) {
+		printf("floor %d\n", floor);
+		return ++floor;
+	}
+	else {
+		printf("result %d\n", floor/2);
+		return (floor/2);	
+	}
 }
